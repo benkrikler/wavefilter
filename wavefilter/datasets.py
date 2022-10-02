ec221cb0d39decc821c835941e2fc4bda01b839f
Benjamin Edward Krikler <4083697+benkrikler@users.noreply.github.com>
2022-10-02 14:24:16 +0200

    Validate double pulse generation via notebook

--- wavefilter/datasets.py
+++ wavefilter/datasets.py
@@ -11,11 +11,11 @@ class ToyData:
     shape: float
     in_noise: float
     out_noise: float
-    generator: Callable[[], List[Tuple[int, float]]]
+    generator: Callable[[int], List[Tuple[int, float]]]
     max_truth_pulses: int = 10
 
     def __call__(self, n_samples: int) -> Tuple[Any, Tuple[Any, Any]]:
-        pulse_length = 3 * self.shape
+        pulse_length = 10 * self.shape
         padded_length = self.length + 2* (pulse_length - 1)
         t0 = pulse_length - 1
 
@@ -41,7 +41,8 @@ class ToyData:
 
         # Convolve with pulse shape
         pulse = simple(np.arange(pulse_length), self.shape)
-        y = convolve1d(y, pulse, origin=pulse_length//2 - 1)
+        pulse /= pulse.max()
+        y = convolve1d(y, pulse, origin=1 - pulse_length//2)
 
         # Add output noise
         y += np.random.normal(scale=self.out_noise, size=data_dims)
@@ -61,15 +62,19 @@ class DoublePulses:
     a_low: float = 0
     a_high: float = 100
 
-    def __call__(self, n_samples) -> List[Tuple[int,float]]:
+    def __call__(self, n_samples: int ) -> List[Tuple[int,float]]:
         t1 = np.random.normal(self.t1_mean, self.t1_std, size=n_samples).astype(int)
         t2 = t1 + np.random.randint(self.dt2_low, self.dt2_high, size=n_samples, dtype=int)
         a1, a2 = np.random.uniform(self.a_low, self.a_high, size=(2, n_samples))
         return [(t1, a1), (t2, a2)]
 
 
-def generate_double_pulse_dataset(n_samples, shape):
-    generator = DoublePulses(t1_mean = 20, t1_std=10, dt2_low=60, dt2_high=150, a_low = 10, a_high=150)
-    builder = ToyData(length=1000, shape=shape, in_noise=3, out_noise=3, generator=generator)
+def generate_double_pulse_dataset(
+        n_samples,
+        t1_mean = 100, t1_std=10, dt2_low=60, dt2_high=150, a_low = 10, a_high=150,
+        length=1000, shape=30, in_noise=1.5, out_noise=3):
+
+    generator = DoublePulses(t1_mean=t1_mean, t1_std=t1_std, dt2_low=dt2_low, dt2_high=dt2_high, a_low=a_low, a_high=a_high)
+    builder = ToyData(length=length, shape=shape, in_noise=in_noise, out_noise=out_noise, generator=generator)
     data, truth = builder(n_samples)
     return data, truth
