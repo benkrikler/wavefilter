3a6157dc5cbe29c1b8555a0fd3560ab701bab328
Benjamin Edward Krikler <4083697+benkrikler@users.noreply.github.com>
2022-10-02 15:14:01 +0200

    Fix typing for pulse_functions

--- wavefilter/pulse_functions.py
+++ wavefilter/pulse_functions.py
@@ -1,22 +1,21 @@
-from typing import Any
-
 import numpy as np
+import numpy.typing as npt
 
 
-def simple(x: Any, tau: float = 10, x0: int = 0) -> Any:
+def simple(x: npt.ArrayLike, tau: float = 10, x0: int = 0) -> npt.NDArray[float]:
     xx = x - x0
     y = xx * np.exp(-xx / tau) / tau / tau
     y[x < x0] = 0
     return y
 
 
-def bimodal(x: Any, tau1: float = 10, tau2: float = 30, x0: int = 0) -> Any:
+def bimodal(x: npt.ArrayLike, tau1: float = 10, tau2: float = 30, x0: int = 0) -> npt.NDArray[float]:
     y1 = simple(x, tau1, x0)
     y2 = simple(x, tau2, x0)
     return y1 - y2
 
 
-def white_noise(x: Any, std: float) -> Any:
+def white_noise(x: npt.ArrayLike, std: float) -> npt.NDArray[float]:
     if isinstance(x, np.ndarray):
         x = len(x)
     return np.random.normal(0, std, x)
