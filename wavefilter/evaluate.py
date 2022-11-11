67f420ae7f0a8b826cbd768ad78b73965a648b80
Benjamin Edward Krikler <4083697+benkrikler@users.noreply.github.com>
2022-11-11 18:25:48 +0100

    Make the train / testing loop more reusable

--- wavefilter/evaluate.py
+++ wavefilter/evaluate.py
@@ -1,5 +1,6 @@
 import logging
-from typing import Any, Callable, Dict, List, Tuple
+from collections import defaultdict
+from typing import Any, Callable, Dict, Iterable, List, Mapping
 
 import numpy as np
 import torch
@@ -13,15 +14,21 @@ logging.basicConfig(level=logging.INFO)
 
 # Based on https://medium.com/nerd-for-tech/convolution-neural-network-in-pytorch-81023e7de5b9
 class TrainTester:
-    def __init__(self, model: Any, optimizer: torch.optim.Optimizer, device: Any, loss: nn.modules.loss._Loss) -> None:
+    def __init__(
+        self,
+        model: Any,
+        optimizer: torch.optim.Optimizer,
+        device: Any,
+        loss: nn.modules.loss._Loss,
+        post_train_step_hooks: Iterable[Callable[[int, Mapping[str, List[float]]], None]] = tuple(),
+    ) -> None:
         self.model = model
         self.loss = loss
         # self.opt = torch.optim.Adam(self.model.parameters(), lr = self.lr)
         self.opt = optimizer
-        self.train_loss: List[float] = []
-        self.val_loss: List[float] = []
-        self.train_acc: List[float] = []
-        self.val_acc: List[float] = []
+        self.post_train_step_hooks = post_train_step_hooks
+        self.train: Mapping[str, List[float]] = defaultdict(list)
+        self.validate: Mapping[str, List[float]] = defaultdict(list)
         self.device = device
 
     def batch_accuracy(self, output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
@@ -31,7 +38,7 @@ class TrainTester:
         acc = torch.sum(output == target) / output.shape[0]
         return acc.cpu() * 100
 
-    def train_step(self, dataset: Any) -> None:
+    def train_step(self, epoch: int, dataset: Any) -> None:
         self.model.train()
         batch_loss = []
         batch_acc = []
@@ -46,11 +53,14 @@ class TrainTester:
             loss = self.loss(outputs, targets)
             loss.backward()
             self.opt.step()
+
             batch_loss.append(loss.item())
             batch_acc.append(self.batch_accuracy(outputs, targets))
 
-        self.train_loss.append(np.mean(batch_loss))
-        self.train_acc.append(np.mean(batch_acc))
+        self.train["loss"].append(np.mean(batch_loss))
+        self.train["acc"].append(np.mean(batch_acc))
+        for hook in self.post_train_step_hooks:
+            hook(epoch, self.train)
 
     def validation_step(self, dataset: Any) -> None:
         self.model.train(False)
@@ -68,8 +78,8 @@ class TrainTester:
                 batch_loss.append(loss.item())
                 batch_acc.append(self.batch_accuracy(outputs, targets))
 
-        self.val_loss.append(np.mean(batch_loss))
-        self.val_acc.append(np.mean(batch_acc))
+        self.validate["loss"].append(np.mean(batch_loss))
+        self.validate["acc"].append(np.mean(batch_acc))
 
     def test_step(self, dataset: Any) -> None:
         self.model.train(False)
@@ -114,29 +124,34 @@ def train_parallel_pulse_finder(
     start_incrementing: int = 40,
     epochs_per_increment: int = 12,
     change_per_increment: float = 0.05,
-) -> Tuple[TrainTester, List[Any]]:
+) -> TrainTester:
 
     optimiser = torch.optim.Adam(param_groups, lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimiser, lr_lambda=lr_schedules)
-    # optimiser = torch.optim.Adam(model.parameters(), lr = learning_rate)
-    train_test_runner = TrainTester(model, optimiser, device, nn.MSELoss())
 
-    adapt_attention = isinstance(model.attend, models.ParallelWeightedModules)
-    learning_rates = []
+    def lr_hook(epoch: int, step_values: Mapping[str, List[float]]) -> None:
+        scheduler.step()
+        lrs = scheduler.get_last_lr()
+        for i, lr in enumerate(lrs):
+            step_values[f"lr_{i}"].append(lr)
+
+    hooks = [lr_hook]
+
+    has_adaptive_attention = isinstance(model.attend, models.ParallelWeightedModules)
+    if has_adaptive_attention:
+
+        def attend_hook(epoch: int, step_values: Mapping[str, List[float]]) -> None:
+            epoch = epoch - start_incrementing
+            if epoch > 0 and (epoch + 1) % epochs_per_increment == 0 and model.attend.get_weight(conv_pf_name) < 1:
+                model.attend.increment_weight(conv_pf_name, change_per_increment)
+                logging.info(epoch, model.attend.module_weights)
+            step_values["pf_weight"].append(model.attend.get_weight(conv_pf_name))
+
+        hooks.append(attend_hook)
+
+    train_test_runner = TrainTester(model, optimiser, device, nn.MSELoss(), post_train_step_hooks=hooks)
 
     for epoch in tqdm(range(epochs), desc="Epoch"):
-        train_test_runner.train_step(data)
-        if (
-            adapt_attention
-            and epoch > start_incrementing
-            and (epoch + 1) % epochs_per_increment == 0
-            and model.attend.get_weight(conv_pf_name) < 1
-        ):
-            model.attend.increment_weight(conv_pf_name, change_per_increment)
-            logging.info(epoch, model.attend.module_weights)
-        scheduler.step()
-        learning_rates.append(scheduler.get_last_lr())
-        # train_test_runner.validation_step(val_loader)
-    # train_test_runner.test_step(test_loader)
+        train_test_runner.train_step(epoch, data)
 
-    return train_test_runner, learning_rates
+    return train_test_runner
